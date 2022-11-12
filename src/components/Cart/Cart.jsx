7be60a84c77cf14f515592c43e535bf120d4eb9b
Vladimir <tiunov2008.va@gmail.com>
2022-11-12 20:07:28 +0300

    fix

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import './Cart.sass';
 import { useSelector } from 'react-redux';
 import { getCart } from './../../store/cart/selectors';
 import { CartItem } from './../CartItem/CartItem';
-import { BooksList, CartList } from './../BooksList/BooksList';
+import { CartList } from './../BooksList/BooksList';
 export const Cart = () => {
     const cart = useSelector((state) => getCart(state));
     return <>
@@ -13,7 +13,7 @@ export const Cart = () => {
         </ul>
         <div class="cart-bottom">
             <span class="cart-sum_title">Итого:</span><span class="cart-sum_value">
-                {Object.entries(cart).map(([key, value]) => <CartItem props={[key, value, true]} />)}
+                {Object.entries(cart).map(([key, value]) => Number(<CartItem props={[key, value, true]} />))}
             </span>
             <button class="cart-button">Купить</button>
         </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 import "./Header.sass"
 export const Header = () => {
     return <header class="header">
-        <span class="header-logo"><Link class="header-logo" to='/'>Магазин</Link></span>
+        <span class="header-logo"><Link class="header-logo" to='/a757a0e9-03c1-4a2a-b384-8ac21dbe2fb2'>Магазин</Link></span>
         <Link to='/cart'><img src={cart} alt="корзина" class="header-cart"/></Link>
     </header>
 }
